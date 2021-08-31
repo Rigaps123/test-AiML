9a360f07618e0aec70f93529662c8db9d5816ab5
Rigaps123 <bhavya@rigaps.com>
2021-08-31 17:06:33 +0530

    yes done

--- assets/js/rig-script.js
+++ assets/js/rig-script.js
@@ -1,11 +1,19 @@
 
         $(function () { 
             $(window).scroll(function () {
-                if ($(this).scrollTop() > 1000) { 
+                if ($(this).scrollTop() > 600) { 
                     $('.navbar-brand img').attr('src','assets/img/new-logo.svg');
+                    $(".main-nav").css("background","#f2f2f2");
+                    $(".nav-link").css("color","#0d1028");
+                    $(".active").css("color","#1dd3f8");
+                    $(".dropdown-menu").css("background","#f2f2f2");
                 }
-                if ($(this).scrollTop() < 1000) { 
+                if ($(this).scrollTop() < 600) { 
                     $('.navbar-brand img').attr('src','assets/img/logo.svg');
+                    $(".main-nav").css("background","none");
+                    $(".nav-link").css("color","#fff");
+                    $(".active").css("color","#1dd3f8");
+                    $(".dropdown-menu").css("background","#0d1028");
                 }
             })
         });
