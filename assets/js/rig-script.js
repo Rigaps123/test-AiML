50fd42b80bde619bbed857bdd80ea6c037aa00b1
Rigaps123 <bhavya@rigaps.com>
2021-09-01 08:18:10 +0530

    mobile change

--- assets/js/rig-script.js
+++ assets/js/rig-script.js
@@ -23,14 +23,14 @@
             $(window).scroll(function () {
                 if ($(this).scrollTop() > 1000) { 
                     $('.logo img').attr('src','assets/img/new-logo.svg');
-                    $(".main-nav").css("background","#fff");
+                    $(".mobile-nav").css("background","#fff");
                     $(".nav-link").css("color","#0d1028");
                     $(".active").css("color","#1dd3f8");
                     $(".dropdown-menu").css("background","#fff");
                 }
                 if ($(this).scrollTop() < 1000) { 
                     $('.logo img').attr('src','assets/img/logo.svg');
-                    $(".main-nav").css("background","none");
+                    $(".mobile-nav").css("background","none");
                     $(".nav-link").css("color","#fff");
                     $(".active").css("color","#1dd3f8");
                     $(".dropdown-menu").css("background","#0d1028");
