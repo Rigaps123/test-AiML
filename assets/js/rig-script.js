0a4807896e4646a6c6a46e52112868e324d57aab
Rigaps123 <bhavya@rigaps.com>
2021-09-07 15:09:00 +0530

    changes done

--- assets/js/rig-script.js
+++ assets/js/rig-script.js
@@ -1,14 +1,14 @@
 
         $(function () { 
             $(window).scroll(function () {
-                if ($(this).scrollTop() > 600) { 
+                if ($(this).scrollTop() > 300) { 
                     $('.navbar-brand img').attr('src','assets/img/new-logo.svg');
                     $(".main-nav").css("background","#fff");
                     $(".nav-link").css("color","#0d1028");
                     $(".active").css("color","#1dd3f8");
                     $(".dropdown-menu").css("background","#fff");
                 }
-                if ($(this).scrollTop() < 600) { 
+                if ($(this).scrollTop() < 300) { 
                     $('.navbar-brand img').attr('src','assets/img/logo.svg');
                     $(".main-nav").css("background","none");
                     $(".nav-link").css("color","#fff");
@@ -21,14 +21,14 @@
 		
 		$(function () { 
             $(window).scroll(function () {
-                if ($(this).scrollTop() > 600) { 
+                if ($(this).scrollTop() > 300) { 
                     $('.logo img').attr('src','assets/img/new-logo.svg');
                     $(".mean-bar").css("background","#fff");
                     $(".nav-link").css("color","#0d1028");
                     $(".active").css("color","#1dd3f8");
                     $(".dropdown-menu").css("background","#fff");
                 }
-                if ($(this).scrollTop() < 600) { 
+                if ($(this).scrollTop() < 300) { 
                     $('.logo img').attr('src','assets/img/logo.svg');
                     $(".mean-bar").css("background","none");
                     $(".nav-link").css("color","#fff");
